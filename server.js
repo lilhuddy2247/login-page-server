136a13b90a767b3200cda360f46fd9ae7bf9fe4b
Hudson DiPierro <102313839+lilhuddy2247@users.noreply.github.com>
2025-12-05 18:54:01 +0000

    Your descriptive commit message here

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    console.log("Received:", username, password);
+
+    // TODO: store/save/do something with the data.
+    // If you want I can show you file saving or database storage.
+
+    res.send("Login received");
+});
+
+app.get("/", (req, res) => {
+    res.send("Render server is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
